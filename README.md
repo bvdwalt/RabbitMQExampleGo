77aeff1b137687ca57b82adfe67e303766a63efa
Bennie van der Walt <bennie@luno.com>
2023-06-05 20:34:16 +0200

    Initial commit of a simple producer and consumer in Go

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # RabbitMQExampleGo
 An example app using RabbitMQ in Go
+
+First run RabbitMQ locally using docker:<br>
+```docker run -d --hostname my-rabbit --name RabbitMQExampleGo -e RABBITMQ_DEFAULT_USER=guest -e RABBITMQ_DEFAULT_PASS=guest -p 15672:15672 -p 5671:5671 -p 5672:5672 -p 15671:15671 -p 25672:25672 rabbitmq:3-management```
+
+Run the two go programs using the following commands:<br>
+The Producer:<br>
+```go run producer/main.go```<br>
+AND the consumer<br>
+```go run consumer/main.go```
---
+++ producer/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+func main() {
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	failOnError(err, "Failed to connect to RabbitMQ")
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+	defer ch.Close()
+
+	q, err := ch.QueueDeclare(
+		"test", // name
+		false,  // durable
+		false,  // delete when unused
+		false,  // exclusive
+		false,  // no-wait
+		nil,    // arguments
+	)
+	failOnError(err, "Failed to declare a queue")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	for i := 1; i <= 100; i++ {
+		body := fmt.Sprintf("Hello World! %v", i)
+		err = ch.PublishWithContext(ctx,
+			"",     // exchange
+			q.Name, // routing key
+			false,  // mandatory
+			false,  // immediate
+			amqp.Publishing{
+				ContentType: "text/plain",
+				Body:        []byte(body),
+			})
+		failOnError(err, "Failed to publish a message")
+		log.Printf(" [x] Sent %s\n", body)
+		// Sleep for 100 Milliseconds before sending the next message onto the queue
+		time.Sleep(100 * time.Millisecond)
+	}
+}
+
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Panicf("%s: %s", msg, err)
+	}
+}
